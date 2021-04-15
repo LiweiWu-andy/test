7cd2bd0bd0889660e84147f9774f1c76f90ed576
LiweiWu-andy <liwei.wu_sx@newsbreak>
2021-04-15 16:01:12 +0800

    调试完成通过postID获取comment

--- main.go
+++ main.go
@@ -11,16 +11,14 @@ type Fetcher = fetcher.Fetcher
 
 var (
 	gFetcher Fetcher
-	gPageFields  = []string{"category", "category_list", "about", "website", "name", "id", "overall_star_rating",
-		"posts.limit(%d){properties,likes.limit(0).summary(true),comments.limit(0).summary(true),message,description,created_time,type,object_id,link,attachments,shares,status_type,parent_id}",
-		"birthday", "artists_we_like", "bio", "band_interests", "is_community_page", "release_date", "is_verified", "talking_about_count",
-		"starring", "season", "produced_by", "genre", "directed_by", "network", "description", "engagement", "fan_count", "link", "location",
-		"username", "verification_status", "picture.type(large)", "emails", "checkins", "price_range", "hours", "phone", "rating_count"}
-	gPostsUrl    = "https://graph.facebook.com/v3.2/%s?fields=%s&access_token=%s"
+	gPageFields  = []string{"like_count", "comment_count", "created_time", "message", "attachment", "from", "comments"}
+	// gPostsUrl2    = "https://graph.facebook.com/v3.2/%s?fields=%s&access_token=%s"
+	gPostsUrl 	 = "https://graph.facebook.com/v3.2/%s/comments?fields=%s&order=%s&limit=%d&access_token=%s"
 	gAccessToken = "153277661752118|mpAYNH_jzO-jwO_PhGexz-xljgg"
 
 	)
 func Fetch(url string) ([]byte, error) {
+	fmt.Println(url)
 	if resp, err := gFetcher.Get(url); err != nil {
 		return nil, err
 	} else {
@@ -35,8 +33,8 @@ func getAllFieldsPage(userName string, num int) ([]byte, error) {
 		userName = ss[len(ss)-1]
 	}
 
-	fields := fmt.Sprintf(strings.Join(gPageFields, ","), num)
-	pageUrl := fmt.Sprintf(gPostsUrl, userName, fields, gAccessToken)
+	fields := strings.Join(gPageFields, ",")
+	pageUrl := fmt.Sprintf(gPostsUrl, userName, fields, "reverse_chronological", 3, gAccessToken)
 	html, err := Fetch(pageUrl)
 	if err != nil {
 		return nil, fmt.Errorf("fetch pageurl error %s", err)
@@ -46,6 +44,6 @@ func getAllFieldsPage(userName string, num int) ([]byte, error) {
 
 func main() {
 	gFetcher = fetcher.NewDefaultFetcher()
-	html, _ := getAllFieldsPage("https://www.facebook.com/1172972172724188", 3)
+	html, _ := getAllFieldsPage("10156401171628788", 3)
 	fmt.Println(string(html))
 }
