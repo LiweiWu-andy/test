757b1a1a59b5df3c08f258a32a4d638538c19e58
LiweiWu-andy <liwei.wu_sx@newsbreak>
2021-04-15 18:23:01 +0800

    解析comment

--- main.go
+++ main.go
@@ -3,20 +3,30 @@ package main
 import (
 	"fmt"
 	"github.com/ParticleMedia/social-fetch/parser/fetcher"
+	sj "github.com/bitly/go-simplejson"
 	"io/ioutil"
 	"strings"
 )
 
+type Comment struct {
+	ID				string		`json:"id"`
+	Message			string		`json:"message"`
+	LikeCount		int			`json:"like_count"`
+	CreateTS		string		`json:"create_ts"`
+}
 type Fetcher = fetcher.Fetcher
 
 var (
 	gFetcher Fetcher
-	gPageFields  = []string{"like_count", "comment_count", "created_time", "message", "attachment", "from", "comments"}
-	// gPostsUrl2    = "https://graph.facebook.com/v3.2/%s?fields=%s&access_token=%s"
+	gCommentFields  = []string{"like_count", "comment_count", "created_time", "message", "attachment", "from", "comments"}
 	gPostsUrl 	 = "https://graph.facebook.com/v3.2/%s/comments?fields=%s&order=%s&limit=%d&access_token=%s"
 	gAccessToken = "153277661752118|mpAYNH_jzO-jwO_PhGexz-xljgg"
 
 	)
+const (
+	commentOrder = "reverse_chronological"
+)
+
 func Fetch(url string) ([]byte, error) {
 	fmt.Println(url)
 	if resp, err := gFetcher.Get(url); err != nil {
@@ -33,8 +43,8 @@ func getAllFieldsPage(userName string, num int) ([]byte, error) {
 		userName = ss[len(ss)-1]
 	}
 
-	fields := strings.Join(gPageFields, ",")
-	pageUrl := fmt.Sprintf(gPostsUrl, userName, fields, "reverse_chronological", 3, gAccessToken)
+	fields := strings.Join(gCommentFields, ",")
+	pageUrl := fmt.Sprintf(gPostsUrl, userName, fields, commentOrder, num, gAccessToken)
 	html, err := Fetch(pageUrl)
 	if err != nil {
 		return nil, fmt.Errorf("fetch pageurl error %s", err)
@@ -42,8 +52,30 @@ func getAllFieldsPage(userName string, num int) ([]byte, error) {
 	return html, nil
 }
 
+func ParseComment (html []byte) ([]Comment, error){
+	comments := make([]Comment, 0)
+	jsonObj, err := sj.NewJson(html)
+	if err != nil {
+		return nil, fmt.Errorf("new json error %s", err)
+	}
+	data := jsonObj.Get("data")
+	for i := range data.MustArray() {
+		comments = append(comments, Comment{
+			data.GetIndex(i).Get("id").MustString(),
+			data.GetIndex(i).Get("message").MustString(),
+			data.GetIndex(i).Get("like_count").MustInt(),
+			data.GetIndex(i).Get("created_time").MustString(),
+		})
+	}
+	return comments, nil
+}
+
 func main() {
 	gFetcher = fetcher.NewDefaultFetcher()
-	html, _ := getAllFieldsPage("10156401171628788", 3)
-	fmt.Println(string(html))
+	html, _ := getAllFieldsPage("10156401171628788", 5)
+	println(string(html))
+	comments, _ := ParseComment(html)
+	for _, comment := range comments {
+		fmt.Println(comment)
+	}
 }
